83b0e645c5e21e807878b08ba7862e86154ee092
arhippa11 <kiitolaj@gmail.com>
2023-05-30 19:30:19 +0300

    Added playlist support and a few extra commands.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pytube import Channel
 from pytube.cli import on_progress
 from sys import argv
 link = ""
+linkglobal = ""
+linkplaylist = ""
 def clean():
     if os.name == "nt":
         os.system("cls")
@@ -25,8 +27,19 @@ def linkgathering():
         sys.exit()
     linkglobal = link
     return linkglobal
-linkglobal = linkgathering()
-clean()
+def playlistlinkgathering():
+    try:
+        link = str(input("Enter the link of the playlist: "))
+        playlist = Playlist(link)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit()
+    except:
+        print("Link is not valid!")
+        print("\nExiting...")
+        sys.exit()
+    linkplaylist = link
+    return linkplaylist
 def maincommandhandler(linkglobal):
     try:
         command = str(input("youtube-dl> "))
@@ -62,12 +75,14 @@ def maincommandhandler(linkglobal):
             clean()
             maincommandhandler(linkglobal)
         elif command == "info":
+            yt = YouTube(linkglobal)
             print("Link: ", linkglobal)
             print("Title: ", yt.title)
             print("Views: ", yt.views)
             print("Length: ", yt.length)
             print("Thumbnail URL: ", yt.thumbnail_url)
             print("Description: ", yt.description)
+            maincommandhandler(linkglobal)
 
         else:
             print("Invalid command!")
@@ -76,6 +91,66 @@ def maincommandhandler(linkglobal):
     except KeyboardInterrupt:
         print("\nExiting...")
         sys.exit()
+def maincommandhandlerplaylist(linkplaylist):
+    try:
+        command = str(input("youtube-dl (playlist mode)> "))
+        if command == "help":
+            print("Commands:")
+            print("help - Displays this message")
+            print("download - Downloads the video")
+            print("exit - Exits the program")
+            print("link - Displays the current link")
+            print("link-change - Changes the current link")
+            maincommandhandlerplaylist(linkplaylist)
+        elif command == "download":
+            print("Downloading...")
+            yt = Playlist(linkplaylist)
+            for video in yt.videos:
+                video.register_on_progress_callback(on_progress)
+                print("\nDownloading: ", video.title)
+                video.streams.get_highest_resolution().download("./YTfolder")
+            print("\nDownloaded!")
+            maincommandhandlerplaylist(linkplaylist)
+        elif command == "exit":
+            print("Exiting...")
+            sys.exit()
+        elif command == "":
+            maincommandhandlerplaylist(linkplaylist)
+        elif command == "link":
+            print(linkplaylist)
+            maincommandhandlerplaylist(linkplaylist)
+        elif command == "link-change":
+            clean()
+            linkplaylist = playlistlinkgathering()
+            maincommandhandlerplaylist(linkplaylist)
+            clean()
+        elif command == "clear":
+            clean()
+            maincommandhandlerplaylist(linkplaylist)
+        elif command == "info":
+            yt = Playlist(linkplaylist)
+            print("Link: ", linkplaylist)
+            print("Title: ", yt.title)
+            maincommandhandlerplaylist(linkplaylist)
 
-maincommandhandler(linkglobal)
+        else:
+            print("Invalid command!")
+            maincommandhandlerplaylist(linkplaylist)
+        
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit()
+
+print("Welcome to youtube-dl!")
+playlistorvideo = str(input("Is it a playlist or a video? (playlist/video): "))
+if playlistorvideo == "playlist":
+    playlistorvideo = "playlist"
+    linkplaylist = playlistlinkgathering()
+elif playlistorvideo == "video":
+    playlistorvideo = "video"
+    linkglobal = linkgathering()
+if playlistorvideo == "playlist":
+    maincommandhandlerplaylist(linkplaylist)
+elif playlistorvideo == "video":
+    maincommandhandler(linkglobal)
 
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-A basic commandline youtube video downloader.
+A basic commandline youtube video downloader for singular videos and playlists(limited support).
 The code is all from arhippa11.
 I use pytube library for the api access and such.
 TODO:
